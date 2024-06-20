7f1874ff4c9aaa21a131f06f556988a330780155
svecx <sinatrakusuma015@gmail.com>
2024-06-21 03:45:36 +0700

    verifikasi mahasiswa dan lain

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,7 +49,7 @@ class LoginController extends Controller
             auth()->logout();
 
             if ($user->jabatan === 'Mahasiswa') {
-                return redirect('/login')->withErrors([
+                return redirect('/2')->withErrors([
                     'email' => 'Akun Anda belum diverifikasi. Mohon untuk menghubungi Kaprodi untuk verifikasi.'
                 ]);
             } else {
